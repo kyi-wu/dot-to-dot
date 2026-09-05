8dbeab0cd30d0f73691754778d1056249a9f9b4f
anwu <you@example.com>
2026-09-05 19:56:20 +0100

    hid game complete message

--- game.js
+++ game.js
@@ -1184,15 +1184,15 @@ function completeLevel() {
         levels.length - 1
     ) {
 
-        messageElement.textContent =
-            "🎉 You completed all levels!";
+        // messageElement.textContent =
+        //     "🎉 You completed all levels!";
 
     }
 
     else {
 
-        messageElement.textContent =
-            "🎉 Level complete!";
+        // messageElement.textContent =
+        //     "🎉 Level complete!";
 
         nextButton.classList.remove(
             "hidden"
