370795c59e5e9bd01e7094230ce58f7964552354
anwu <you@example.com>
2026-09-08 00:29:09 +0100

    level 2-5 dots added

--- levels.js
+++ levels.js
@@ -121,10 +121,10 @@ const levels = [
         image: "images/level2.png",
         // Images shown before the player starts connecting
         beforeConnectionImage:
-            "images/level2_before_connection_image.jpg",
+            "images/level2_before.png",
 
         beforeConnectionText:
-            "images/level2_before_connection_text.png",
+            "images/level2_text.png",
             
         showNumbers: true,
             // -------------------------------------
@@ -145,16 +145,22 @@ const levels = [
         },
 
     points: [
-            { x: 442, y: 157 },
-            { x: 304, y: 391 },
-            { x: 264, y: 506 },
-            { x: 372, y: 559 },
-            { x: 496, y: 967 },
-            { x: 586, y: 557 },
-            { x: 642, y: 501 },
-            { x: 602, y: 426 },
-            { x: 596, y: 340 },
-            { x: 496, y: 277 }
+            { x: 840, y: 147 },
+            { x: 728, y: 93 },
+            { x: 666, y: 120 },
+            { x: 623, y: 118 },
+            { x: 503, y: 296 },
+            { x: 548, y: 393 },
+            { x: 552, y: 422 },
+            { x: 642, y: 496 },
+            { x: 671, y: 470 },
+            { x: 751, y: 547 },
+            { x: 850, y: 641 },
+            { x: 932, y: 661 },
+            { x: 1037, y: 565 },
+            { x: 1024, y: 422 },
+            { x: 948, y: 230 },
+            { x: 922, y: 149 }
     ]
     },
 
@@ -168,10 +174,10 @@ const levels = [
 
         // Images shown before the player starts connecting
         beforeConnectionImage:
-            "images/level3_before_connection_image.jpg",
+            "images/level3_before.png",
 
         beforeConnectionText:
-            "images/level3_before_connection_text.png",
+            "images/level3_text.png",
 
         showNumbers: true,
 
@@ -194,17 +200,18 @@ const levels = [
 
         points: [
 
-            { x: 400, y: 100 },
-            { x: 450, y: 180 },
-            { x: 520, y: 220 },
-            { x: 470, y: 280 },
-            { x: 500, y: 370 },
-            { x: 400, y: 330 },
-            { x: 300, y: 370 },
-            { x: 330, y: 280 },
-            { x: 280, y: 220 },
-            { x: 350, y: 180 }
-
+            { x: 735, y: 486 },
+            { x: 760, y: 649 },
+            { x: 682, y: 657 },
+            { x: 711, y: 904 },
+            { x: 752, y: 947 },
+            { x: 764, y: 1038 },
+            { x: 711, y: 1064 },
+            { x: 831, y: 1069 },
+            { x: 783, y: 1033 },
+            { x: 780, y: 949 },
+            { x: 828, y: 889 },
+            { x: 840, y: 651 }
         ]
     },
 
@@ -218,10 +225,10 @@ const levels = [
 
         // Images shown before the player starts connecting
         beforeConnectionImage:
-            "images/level4_before_connection_image.jpg",
+            "images/level4_before.png",
 
         beforeConnectionText:
-            "images/level4_before_connection_text.png",
+            "images/level4_text.png",
 
 
         showNumbers: true,
@@ -245,36 +252,99 @@ const levels = [
 
         points: [
 
-            { x: 200, y: 120 },
-            { x: 280, y: 100 },
-            { x: 360, y: 130 },
-            { x: 440, y: 100 },
-            { x: 520, y: 150 },
-            { x: 570, y: 230 },
-            { x: 540, y: 310 },
-            { x: 470, y: 360 },
-            { x: 400, y: 400 },
-            { x: 320, y: 360 },
-            { x: 250, y: 400 },
-            { x: 180, y: 330 },
-            { x: 140, y: 240 }
+          { x: 589, y: 875 },
+            { x: 504, y: 846 },
+            { x: 455, y: 774 },
+            { x: 432, y: 771 },
+            { x: 460, y: 845 },
+            { x: 424, y: 816 },
+            { x: 370, y: 745 },
+            { x: 357, y: 760 },
+            { x: 292, y: 741 },
+            { x: 386, y: 825 },
+            { x: 431, y: 859 },
+            { x: 359, y: 843 },
+            { x: 335, y: 891 },
+            { x: 440, y: 888 },
+            { x: 522, y: 909 }
 
         ]
     },
 
+
+    
     // =====================================
-    // LEVEL 5 — END
+    // LEVEL 5
+    // =====================================
+
+    {
+        image: "images/level5.png",
+
+        // Images shown before the player starts connecting
+        beforeConnectionImage:
+            "images/level5_before.png",
+
+        beforeConnectionText:
+            "images/level5_text.png",
+
+
+        showNumbers: true,
+
+        // -------------------------------------
+        // LEVEL COLORS
+        // -------------------------------------
+        colors: {
+            point: "#FFFFFF",
+            pointStroke: "#333333",
+
+            pointHover: "#EEEEEE",
+
+            completedPoint: "#4CAF50",
+            completedPointStroke: "#388E3C",
+
+            line: "#4CAF50",
+
+            number: "#333333"
+        },
+
+        points: [
+            { x: 429, y: 161 },
+            { x: 400, y: 174 },
+            { x: 388, y: 251 },
+            { x: 320, y: 305 },
+            { x: 314, y: 345 },
+            { x: 260, y: 441 },
+            { x: 264, y: 502 },
+            { x: 324, y: 536 },
+            { x: 420, y: 539 },
+            { x: 442, y: 512 },
+            { x: 506, y: 505 },
+            { x: 533, y: 533 },
+            { x: 600, y: 517 },
+            { x: 616, y: 449 },
+            { x: 592, y: 411 },
+            { x: 594, y: 366 },
+            { x: 516, y: 297 },
+            { x: 469, y: 269 },
+            { x: 450, y: 238 },
+            { x: 453, y: 184 }
+        ]
+    },
+
+
+    // =====================================
+    // LEVEL 6 — END
     // =====================================
 
     {
         image:
-            "images/level5.png",
+            "images/level6.png",
 
         beforeConnectionImage:
-            "images/level5_before_connection_image.png",
+            "images/level6_before.png",
 
         beforeConnectionText:
-            "images/level5_before_connection_text.png",
+            "images/level6_text.png",
 
         showNumbers: true,
 
